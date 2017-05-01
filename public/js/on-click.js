e6580240ab0a2bbc8a47b16dd82eda8d2c641146
curtisgreene <curtis.j.w.greene@gmail.com>
2017-05-01 18:26:56 -0400

    my solution

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -3,4 +3,9 @@
 $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
+  $("#load-cars").on("click", function(e){
+    //fetchJSON()
+    console.log("I was clicked")
+    fetchJSON()
+  });
 });
